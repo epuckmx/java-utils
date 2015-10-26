26513f043005895563abccacfe601296d149bde2
Nelson Iván González <nigm2005@gmail.com>
2015-10-25 18:20:25 -0600

    Check record

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
 
     public static final int WIDTH = 40;
     public static final int HEIGHT = 40;
+    public static final int MAX_OBJECTS = 5;
+    public static final int MIN_HEIGHT = 3;
+    public static final int MIN_WIDTH = 3;
 
     enum Channel {
         RED,
@@ -43,12 +46,13 @@ public class Main {
 
     }
 
+    // x, y is the center of the rectangle
     static class Rect {
 
-        int x;
-        int y;
-        int w;
-        int h;
+        int x = 0;
+        int y = 0;
+        int w = 0;
+        int h = 0;
 
     }
 
@@ -145,7 +149,7 @@ public class Main {
             switch (channel) {
                 case RED:
                     int r = (image[i].r - image[i].g) + (image[i].r - image[i].b);
-                    binaryRed = (r > 40) ? 255 : 0;
+                    binaryRed = (r > 30) ? 255 : 0;
                     break;
                 case GREEN:
                     int g = (image[i].g - image[i].r) + (image[i].g - image[i].b);
@@ -268,7 +272,7 @@ public class Main {
 
     public static Rect getRectForImage(RGB[] image, Channel channel) {
         int iteration = 0;
-        while (iteration < WIDTH * HEIGHT / 2) {
+        while (iteration < WIDTH * HEIGHT) {
             int pixel = (int)Math.floor(Math.random() * WIDTH * HEIGHT);
             int x = pixel % WIDTH;
             int y = pixel / WIDTH;
@@ -278,10 +282,58 @@ public class Main {
             while (newArea > area) {
                 area = newArea;
                 if (getColorForPixel(image, x, y, channel) > 0) {
-                    rect = sDistribution[pixel];
-                    newArea = rect.w * rect.h;
-                    x = rect.x;
-                    y = rect.y;
+                    pixel = y * WIDTH + x;
+                    Rect mRect = sDistribution[pixel];
+                    if (mRect.w > MIN_WIDTH && mRect.h > MIN_HEIGHT) {
+                        int mArea;
+                        int maxArea;
+                        int maxPixel;
+                        // Check points
+                        // center
+                        mArea = mRect.w * mRect.h;
+                        maxArea = mArea;
+                        maxPixel = pixel;
+                        rect = mRect;
+                        // top left
+                        int tlPixel = (mRect.y - mRect.h / 4) * WIDTH + (mRect.x - mRect.w / 4);
+                        Rect tlRect = sDistribution[tlPixel];
+                        mArea = tlRect.w * tlRect.h;
+                        if (mArea > maxArea) {
+                            maxArea = mArea;
+                            maxPixel = tlPixel;
+                            rect = tlRect;
+                        }
+                        // top right
+                        int trPixel = (mRect.y - mRect.h / 4) * WIDTH + (mRect.x + mRect.w / 4);
+                        Rect trRect = sDistribution[trPixel];
+                        mArea = trRect.w * trRect.h;
+                        if (mArea > maxArea) {
+                            maxArea = mArea;
+                            maxPixel = trPixel;
+                            rect = trRect;
+                        }
+                        // bottom left
+                        int blPixel = (mRect.y + mRect.h / 4) * WIDTH + (mRect.x - mRect.w / 4);
+                        Rect blRect = sDistribution[blPixel];
+                        mArea = blRect.w * blRect.h;
+                        if (mArea > maxArea) {
+                            maxArea = mArea;
+                            maxPixel = blPixel;
+                            rect = blRect;
+                        }
+                        // bottom right
+                        int brPixel = (mRect.y + mRect.h / 4) * WIDTH + (mRect.x + mRect.w / 4);
+                        Rect brRect = sDistribution[brPixel];
+                        mArea = brRect.w * brRect.h;
+                        if (mArea > maxArea) {
+                            maxArea = mArea;
+                            maxPixel = brPixel;
+                            rect = brRect;
+                        }
+                        newArea = maxArea;
+                        x = maxPixel % WIDTH;
+                        y = maxPixel / WIDTH;
+                    }
                 }
             }
             if (rect != null) {
@@ -290,6 +342,32 @@ public class Main {
             iteration++;
         }
         return null;
+    }
+
+    public static void deleteRectFromImage(RGB[] image, Rect rect) {
+        int x = rect.x - rect.w / 2;
+        int y = rect.y - rect.h / 2;
+        for (int i = 0; i <= rect.w; ++i) {
+            for (int j = 0; j <= rect.h; ++j) {
+                int index = (y + j) * WIDTH + (x + i);
+                image[index] = new RGB();
+            }
+        }
+    }
+
+    public static List<Rect> getRectsForImage(RGB[] image, Channel channel) {
+        List<Rect> rects = new ArrayList<>();
+        int iteration = 0;
+        while (iteration < MAX_OBJECTS) {
+            Rect rect = getRectForImage(image, channel);
+            if (rect != null) {
+                rects.add(rect);
+                deleteRectFromImage(image, rect);
+                getDistributionImage(image, channel);
+            }
+            iteration++;
+        }
+        return rects;
     }
 
     public static RGB[] granularityFilter(RGB[] data, Channel channel) {
@@ -476,12 +554,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        if (args.length < 2) {
-            System.exit(0);
-        }
-        String filename = args[0];
-        int frameToLook = Integer.parseInt(args[1]);
+    public static void checkFrame(String filename, int frameToLook) {
         InputStream inputStream = Main.class.getResourceAsStream(filename);
         Scanner scanner = new Scanner(inputStream);
         int index = 0;
@@ -499,27 +572,90 @@ public class Main {
             }
         }
         RGB[] image = getImage(data);
-        saveImage(image, 3, "frame");
-        RGB[] average = getAverageImage(image);
         RGB[] median = getMedianImage(image);
         RGB[] binary = getBinaryImage(median, Channel.RED);
+        saveImage(binary, 3, "binary");
         RGB[] granularity = granularityFilter(binary, Channel.RED);
-        RGB[] distribution = getDistributionImage(granularity, Channel.RED);
-        Rect rect = getRectForImage(granularity, Channel.RED);
-        if (rect != null) {
-            drawRectangleInImage(image, rect.x, rect.y, rect.w, rect.h);
-            saveImage(image, 3, "frame_detected");
+        saveImage(granularity, 3, "granularity");
+        int objects = 1;
+        int iterations = 1;
+        int sum = 0;
+        for (int i = 0; i < iterations; i++) {
+            RGB[] copy = Arrays.copyOf(granularity, granularity.length);
+            getDistributionImage(copy, Channel.RED);
+            List<Rect> rects = getRectsForImage(copy, Channel.RED);
+            System.out.println("Detected " + rects.size());
+            for (Rect rect : rects) {
+                System.out.println("w = " + rect.w + " h = " + rect.h);
+                drawRectangleInImage(image, rect.x, rect.y, rect.w, rect.h);
+            }
+            if (rects.size() == objects) {
+                sum++;
+            }
         }
-        saveImage(average, 3, "frame_average");
-        saveImage(median, 3, "frame_median");
-        saveImage(binary, 3, "frame_binary");
-        saveImage(granularity, 3, "frame_granularity");
-        saveImage(distribution, 3, "frame_distribution");
-        saveHistogram(getHistogram(image, Channel.RED), "histogram");
-        saveHistogram(getHistogram(average, Channel.RED), "histogram_average");
-        saveHistogram(getHistogram(median, Channel.RED), "histogram_median");
+        saveImage(image, 3, "detected");
+        if (sum > 0) {
+            System.out.println("Correct " + ((double) sum / iterations));
+        } else {
+            System.out.println("Method sucks!");
+        }
+    }
 
-        //generateFramesFromFile(args[0]);
+    public static void checkRecord(String filename) {
+        InputStream inputStream = Main.class.getResourceAsStream(filename);
+        Scanner scanner = new Scanner(inputStream);
+        int index = 0;
+        int frame = 0;
+        int[] data = new int[2 * WIDTH * HEIGHT];
+        double x0 = 22.0;
+        double y0 = 0.0;
+        while (scanner.hasNextInt()) {
+            data[index++] = scanner.nextInt();
+            if (index == data.length) {
+                RGB[] image = getImage(data);
+                RGB[] median = getMedianImage(image);
+                RGB[] binary = getBinaryImage(median, Channel.RED);
+                RGB[] granularity = granularityFilter(binary, Channel.RED);
+                getDistributionImage(granularity, Channel.RED);
+                List<Rect> rects = getRectsForImage(granularity, Channel.RED);
+                if (rects.size() > 0) {
+                    System.out.println("Frame " + frame + " detected " + rects.size());
+                    for (Rect rect : rects) {
+                        System.out.println("w = " + rect.w + " h = " + rect.h);
+                        double ratio = (double)rect.w / rect.h;
+                        if (ratio < 1.1) {
+                            int w = rect.w;
+                            rect.w = (int)(1.33 * rect.h);
+                            if (rect.x > 20) {
+                                rect.x += (rect.w - w) / 2;
+                            } else {
+                                rect.x -= (rect.w - w) / 2;
+                            }
+                        }
+                        System.out.println("Ratio w / h " + ratio);
+                        double distance = 500.0 / rect.h;
+                        double direction = 1.5 * (rect.x - 20);
+                        double x = x0 + distance * Math.tan(Math.toRadians(direction));
+                        double y = distance;
+                        x /= 10.0;
+                        y /= 10.0;
+                        //String coord = String.format("\\draw [fill] (%.2f, %.2f) circle [radius=0.1];", x, y);
+                        //System.out.println(coord);
+                        System.out.println("Distance " + distance + " direction " + direction);
+                        System.out.println("");
+                    }
+                }
+                index = 0;
+                frame++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String filename = "record2";
+        int frameToLook = 26;
+        checkFrame(filename, frameToLook);
+        //checkRecord(filename);
     }
 
 }
